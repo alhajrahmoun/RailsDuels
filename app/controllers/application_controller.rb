305ac640196933025022255630ee279b206d244a
Mohamad <mrahmoun93@gmail.com>
2023-07-29 05:00:02 +0200

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :static_page?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
+  def static_page?
+    is_a?(StaticPagesController)
+  end
 end
