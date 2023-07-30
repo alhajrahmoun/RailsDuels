70a52e6582a5c200859b5bbc5740b55c87990ce1
Mohamad <mrahmoun93@gmail.com>
2023-07-30 19:02:54 +0200

    more improvements

--- app/helpers/duel_helper.rb
+++ app/helpers/duel_helper.rb
@@ -6,7 +6,7 @@ module DuelHelper
     remaining_percentage = 100 - correct_answers_percentage - wrong_answers_percentage
 
     content_tag(:div, class: 'progress border border-dark') do
-      content_tag(:div, "#{points} Points", class: 'progress-bar bg-success', style: "width: #{correct_answers_percentage}%") +
+      content_tag(:div, "#{points} #{'Point'.pluralize(points)}", class: 'progress-bar bg-success', style: "width: #{correct_answers_percentage}%") +
         content_tag(:div, '', class: 'progress-bar bg-danger progress-bar-stripped', style: "width: #{wrong_answers_percentage}%") +
           content_tag(:div, '', class: 'progress-bar bg-white', style: "width: #{remaining_percentage}%")
     end
