c05922ae6f28c664adabded4362f401d292e57f3
Mohamad <mrahmoun93@gmail.com>
2023-08-05 03:28:16 +0200

    fix spaces

--- app/models/duel_problem.rb
+++ app/models/duel_problem.rb
@@ -2,5 +2,5 @@ class DuelProblem < ApplicationRecord
   belongs_to :duel
   belongs_to :problem
 
-  scope :answered, -> (duel) { joins(problem: :submissions).where(submissions: { duel: duel }) }
+  scope :answered, ->(duel) { joins(problem: :submissions).where(submissions: { duel: duel }) }
 end
